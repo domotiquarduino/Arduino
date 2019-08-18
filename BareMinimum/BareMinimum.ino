9025e9e1c02ffe367a760e887b0d67e2ccb7dbed
Domotiquarduino <53947348+domotiquarduino@users.noreply.github.com>
2019-08-19 01:12:11 +0200

    Add files via upload

---
+++ BareMinimum/BareMinimum.ino
@@ -0,0 +1,11 @@
+void setup() 
+{
+  // mettez votre code principal ici, pour l'exécuter une fois : 
+
+}
+
+void loop() 
+{
+  // mettez votre code principal ici, pour exécuter à plusieurs reprises :
+
+}
